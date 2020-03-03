b85902556cd4d70098366b7b27c7e1a97aab1430
Mark S. Hisle <mark.hisle@centre.edu>
2020-03-03 14:49:58 -0500

    additional storyline and styling

--- interactions.js
+++ interactions.js
@@ -4,9 +4,124 @@ var changePic = function(that,picName)
         .attr("src", picName);
 }
 
+var overflow = function()
+{
+    d3.select("body")
+      .style("background-image", "url('castle_background.webp')");
+    
+    d3.select("body")
+      .style("color", "black");
+    
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You win!");
+    d3.select("body")
+      .append("img")
+        .attr("src", "crown.jpg");
+    d3.select("body")
+      .append("h3").text("Luckily for you, a more experienced programmer has already made a solution to slaying dragons. Thank goodness for the Internet!");
+}
+
+var magic = function()
+{
+    d3.select("body")
+      .style("background-image", "url('castle_background.webp')");
+    
+    d3.select("body")
+      .style("color", "black");
+    
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You win!");
+    d3.select("body")
+      .append("img")
+        .attr("src", "crown.jpg");
+    d3.select("body")
+      .append("h3").text("Congratulations, wizard! You have banished the dragon from this realm. Students of Hogwarts will hear of your bravery for generations to come.");
+}
+
+var charge = function()
+{
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You lose...");
+    d3.select("body")
+      .append("img")
+        .attr("src", "dead.webp");
+    d3.select("body")
+      .append("h3").text("While charging the dragon, you trip on a unfortunately placed rock and swiftly become dragon food.");
+}
+
+var runaway = function()
+{
+    d3.select("body")
+      .style("background-image", "url('forest_background.jpg')");
+    
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You lose...");
+    d3.select("body")
+      .append("img")
+        .attr("src", "dragon-36748_960_720.png");
+    d3.select("body")
+      .append("h3").text("You ran away, only to be forgotten by history. For accomplishing nothing. But you lived so good for you.");
+}
+
+var fightProgrammer = function()
+{
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You lose...");
+    d3.select("body")
+      .append("img")
+        .attr("src", "dead.webp");
+    d3.select("body")
+      .append("h3").text("You died! Really? You thought a computer programmer could fight a dragon?");
+}
+
+var fightWizard = function()
+{
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You lose...");
+    d3.select("body")
+      .append("img")
+        .attr("src", "dead.webp");
+    d3.select("body")
+      .append("h3").text("You died! Unfortunately for you, wizards are not skilled at hand-to-hand combat. With dragons. Bad choice.");
+}
+
+var fightKnight = function()
+{
+    d3.select("body")
+      .style("background-image", "url('castle_background.webp')");
+    
+    d3.select("body")
+      .style("color", "black");
+    
+    d3.selectAll("img").remove();
+    d3.selectAll("p").remove();
+    d3.select("body")
+      .text("You win!");
+    d3.select("body")
+      .append("img")
+        .attr("src", "crown.jpg");
+    d3.select("body")
+      .append("h3").text("You have slain the dragon, brave knight! History will forever remember you for your great deeds to mankind.");
+}
+
 
 var part2Knight = function()
 {
+    d3.select("body")
+      .style("background-image", "url('cave_background.jpg')");
+    
     d3.select("p")
       .append("h2")
         .text("Choose your action!");
@@ -23,11 +138,12 @@ var part2Knight = function()
           {
             d3.select(this)
               .attr("src", "run_away.png")
-    }        
-           
-           
-           
-           );
+    })
+       .on("click", function()
+           {
+            runaway();
+    })
+    ;
     d3.select("p")
         .append("img")
         .attr("src","fight.png")
@@ -41,7 +157,10 @@ var part2Knight = function()
             d3.select(this)
               .attr("src", "fight.png")
     }        
-    );
+    ).on("click", function()
+           {
+            fightKnight();
+    });
     d3.select("p")
         .append("img")
         .attr("src","charge.png")
@@ -56,11 +175,17 @@ var part2Knight = function()
               .attr("src", "charge.png")
     }        
     
-    );
+    ).on("click", function()
+           {
+            charge();
+    });
 }
 
 var part2Wizard = function()
 {
+    d3.select("body")
+      .style("background-image", "url('cave_background.jpg')");
+    
     d3.select("p")
       .append("h2")
         .text("Choose your action!");
@@ -78,7 +203,10 @@ var part2Wizard = function()
             d3.select(this)
               .attr("src", "run_away.png")
     }        
-    );
+    ).on("click", function()
+           {
+            runaway();
+    });
     d3.select("p")
         .append("img")
         .attr("src","fight.png")
@@ -94,7 +222,10 @@ var part2Wizard = function()
     }
     
     
-    );
+    ).on("click", function()
+           {
+            fightWizard();
+    });
     d3.select("p")
         .append("img")
         .attr("src","magic.png")
@@ -108,12 +239,17 @@ var part2Wizard = function()
             d3.select(this)
               .attr("src", "magic.png")
     }
-    
-    );
+    ).on("click", function()
+           {
+            magic();
+    });
 }
 
 var part2Programmer = function()
 {
+    d3.select("body")
+      .style("background-image", "url('cave_background.jpg')");
+    
     d3.select("p")
       .append("h2")
         .text("Choose your action!");
@@ -133,7 +269,10 @@ var part2Programmer = function()
     }        
     
     
-    );
+    ).on("click", function()
+           {
+            runaway();
+    });
     d3.select("p")
         .append("img")
         .attr("src","fight.png")
@@ -148,7 +287,10 @@ var part2Programmer = function()
               .attr("src", "fight.png")
     }
     
-    );
+    ).on("click", function()
+           {
+            fightProgrammer();
+    });
     d3.select("p")
         .append("img")
         .attr("src","stack_overflow.png")
@@ -163,7 +305,10 @@ var part2Programmer = function()
               .attr("src", "stack_overflow.png")
     }
     
-    );
+    ).on("click", function()
+           {
+            overflow();
+    });
 }
 
 
